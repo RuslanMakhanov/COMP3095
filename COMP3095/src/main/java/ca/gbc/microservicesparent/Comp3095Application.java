4c5f6df8c9b01da78eef30d1cb238da7707f80e9
RuslanNnNnNn <95371964+RuslanMakhanov@users.noreply.github.com>
2023-11-06 20:27:29 -0500

    first commit

---
+++ COMP3095/src/main/java/ca/gbc/microservicesparent/Comp3095Application.java
@@ -0,0 +1,13 @@
+package ca.gbc.microservicesparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3095Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp3095Application.class, args);
+	}
+
+}
---
+++ COMP3095/src/test/java/ca/gbc/microservicesparent/Comp3095ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.microservicesparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp3095ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
